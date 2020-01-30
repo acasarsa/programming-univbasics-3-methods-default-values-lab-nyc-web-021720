58d0f3f0e44bc4f873d8ef95652fac7c21bb64b4
Andrew J Casarsa <acasarsa@gmail.com>
2020-01-30 18:05:04 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 def meal_choice (veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  return "A plate of #{protein} with #{veg1} and #{veg2}." 
+  "A plate of #{protein} with #{veg1} and #{veg2}." 
 end
 # I don't need to type return cs ruby implicitly will return the last executed instruction in your method
